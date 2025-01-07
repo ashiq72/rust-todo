b0459f664577ab19bf10e21446a4d44c82422267
ashiq72 <ahmedashik18k@gmail.com>
2025-01-07 22:44:41 +0600

    Updated react state

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdDelete, MdEdit, MdConfirmationNumber } from "react-icons/md";
+import axios from "axios";
+import { format } from "date-fns";
 
 const index = () => {
+  const [editText, setEditText] = useState();
+  const [todos, setTodos] = useState([]);
+  const [todosCopy, setTodosCopy] = useState(todos);
+  const [todoInput, setTodoInput] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResult] = useState([]);
+
   return <div>index</div>;
 };
 
