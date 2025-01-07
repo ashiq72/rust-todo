63b02fcfc49951f89298f057d0a482f3f12440e3
ashiq72 <ahmedashik18k@gmail.com>
2025-01-07 23:02:28 +0600

    Created fetchTodos function

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,16 @@ const index = () => {
     setEditIndex(index);
   };
 
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8080/todos");
+      setTodos(response.data);
+      setTodosCopy(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>index</div>;
 };
 
